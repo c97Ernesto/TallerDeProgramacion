f97dcd6999f7e3da4f4fbab2c21202394a7f93f2
c97Ernesto <ernesto-cafre@hotmail.com>
2022-08-17 16:23:02 -0300

    tallerProgramacion, modulo objetos, tp2021ProyectoAlumnos, ejer3 y 4

--- Practica2-Objetos/TP2021_ProyectoAlumnos/src/tema1/Ejercicio3_AlmacenarEnVector.java
+++ Practica2-Objetos/TP2021_ProyectoAlumnos/src/tema1/Ejercicio3_AlmacenarEnVector.java
@@ -1,8 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*3_Escriba un programa que lea las alturas de los 15 jugadores de un equipo de 
+    básquet y las almacene en un vector. Luego informe: 
+        - la altura promedio
+        - la cantidad de jugadores con altura por encima del promedio
+*/
 package tema1;
     import PaqueteLectura.Lector;
 /**
@@ -12,23 +12,26 @@ package tema1;
 public class Ejercicio3_AlmacenarEnVector {
      public static void main(String[] args) {
         double [] vecAltura;//Paso 2: Declarar la variable vector de double 
+        int maxAlturas = 3;
         
-        vecAltura = new double [15];//Paso 3: Crear el vector para 15 double 
+        vecAltura = new double [maxAlturas];//Paso 3: Crear el vector para 15 double 
         
         int i;//Paso 4: Declarar indice 
         double suma = 0;
+        double altura;
         
-        for (i = 0; i < 15; i++){ //Paso 6: Ingresar 15 numeros (altura), cargarlos en el vector, ir calculando la suma de alturas
-            double altura = Lector.leerDouble();
+        for (i = 0; i < maxAlturas; i++){ //Paso 6: Ingresar 15 numeros (altura), cargarlos en el vector, ir calculando la suma de alturas
+            System.out.print("Ingresar altura: ");
+            altura = Lector.leerDouble();
             vecAltura [i] = altura;
             suma = suma + altura;
         }
         
-        double prom = suma / 15;//Paso 7: Calcular el promedio de alturas, informarlo
+        double prom = suma / maxAlturas;//Paso 7: Calcular el promedio de alturas, informarlo
         System.out.println("El promedio de Alturas es: " +prom);
         
         int cant = 0;
-        for (i = 0; i < 15; i++)   //Paso 8: Recorrer el vector calculando lo pedido (cant. alturas que están por encima del promedio)
+        for (i = 0; i < maxAlturas; i++)   //Paso 8: Recorrer el vector calculando lo pedido (cant. alturas que están por encima del promedio)
             if (vecAltura[i] > prom)
                 cant = cant + 1;
         
--- Practica2-Objetos/TP2021_ProyectoAlumnos/src/tema1/Ejercicio4_MatrizDeEnteros.java
+++ Practica2-Objetos/TP2021_ProyectoAlumnos/src/tema1/Ejercicio4_MatrizDeEnteros.java
@@ -1,7 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*4_Escriba un programa que defina una matriz de enteros de tamaño 10x10. 
+    Inicialice la matriz con números aleatorios entre 0 y 200. 
+    Luego realice las siguientes operaciones: 
+        - Mostrar el contenido de la matriz en consola. 
+        - Calcular e informar la suma de todos los elementos almacenados entre las 
+        filas 2 y 9 y las columnas 0 y 3
+        - Generar un vector de 10 posiciones donde cada posición i contiene la suma 
+        de los elementos de la columna i de la matriz.
+        - Lea un valor entero e indique si se encuentra o no en la matriz. En caso de 
+        encontrarse indique su ubicación (fila y columna) en caso contrario 
+        imprima “No se encontró el elemento”.
  */
 package tema1;
 
