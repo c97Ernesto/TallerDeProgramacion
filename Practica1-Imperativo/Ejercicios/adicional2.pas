f1e3b04b6ec0e31dbb59be1eaf968f438d14998f
c97Ernesto <ernesto-cafre@outlook.com>
2023-08-15 16:04:50 -0300

    add

--- Practica1-Imperativo/Ejercicios/adicional2.pas
+++ Practica1-Imperativo/Ejercicios/adicional2.pas
@@ -1,61 +1,70 @@
 {2. Un teatro tiene funciones los 7 días de la semana. Para cada día se tiene una 
-lista con las entradas vendidas.
-Se desea procesar la información de una semana. Se pide:
-a. Generar 7 listas con las entradas vendidas para cada día. De cada entrada se lee día (de
-1 a 7), código de la obra, asiento, monto. La lectura finaliza con el código de obra igual
-a 0. Las listas deben estar ordenadas por código de obra de forma ascendente.
-b. Generar una nueva lista que totalice la cantidad de entradas vendidas por obra. Esta
-lista debe estar ordenada por código de obra de forma ascendente.
-c. Realice un módulo recursivo que informe el contenido de la lista generada en b)}
+lista con las entradas vendidas. Se desea procesar la información de una semana. 
+Se pide:
+    a. Generar 7 listas con las entradas vendidas para cada día. De cada entrada 
+    se lee día (de 1 a 7), código de la obra, asiento, monto. La lectura finaliza 
+    con el código de obra igual a 0. Las listas deben estar ordenadas por código 
+    de obra de forma ascendente.
+    b. Generar una nueva lista que totalice la cantidad de entradas vendidas por 
+    obra. Esta lista debe estar ordenada por código de obra de forma ascendente.
+    c. Realice un módulo recursivo que informe el contenido de la lista generada 
+    en b)
+    
+}
 
 PROGRAM Adicional2;
 CONST
-	MAXdias = 7;
+    VALOR_ALTO = 9999;
+	MAX_dias = 7;
+	FIN = 0;
+	
 TYPE
-	rng_dias = 1..MAXdias;
+	rango_dias = 1..MAX_dias;
 	
 	{punto a}
 	reg_entrada = record
-		dia: rng_dias;
+		dia: rango_dias;
 		codObra: integer;
 		asiento: integer;
 		monto: real;
 	end;
 	
-	lista_entradas = ^nodoE;
-	nodoE = record
+	lista_entradas = ^nodo_entrada;
+	nodo_entrada = record
 		datos: reg_entrada;
-		sig: lista;
+		sig: lista_entradas;
 	end;
 	
-	vec_dias = array [rng_dias] of lista;
+	vec_dias = array [rango_dias] of lista_entradas;
 	
 	{punto b}
-	reg_obra = record;
+	reg_obra = record
 		codObra: integer;
 		entradasVendidas: integer;
 	end;
 	
-	lista_obras = ^nodoO;
-	nodoO = record
+	lista_obras = ^nodo_obra;
+	nodo_obra = record
 		datos: reg_obra;
-		sig: lista;
+		sig: lista_obras;
 	end;
 	
 //__________________________________IniVectorListas__________________________________
 Procedure InicializarVec(var v: vec_dias);
+Var
+    i: rango_dias;
 Begin
-	for i:= 1 to MAXdias do 
+	for i:= 1 to MAX_dias do 
 		v[i]:= nil;
 End;
 //__________________________________GenerarVectorListas__________________________________
-Procedure GenerarVec(var vec: vec_dias);
+Procedure GenerarVec(var vDias: vec_dias);
 	
-	procedure leerReg(r: reg_entrada);
+	procedure leerReg(var r: reg_entrada);
 	begin
 		with r do begin
 			readln(codObra);
-			if (codObra <> 0) then begin
+			if (codObra <> fin) then begin
 				readln(dia);
 				readln(asiento);
 				readln(monto);
@@ -68,14 +77,14 @@ Procedure GenerarVec(var vec: vec_dias);
 		ant, nodo, act: lista_entradas;
 	begin
 		new(nodo);
-		nodo^.datos:= reg_entrada;
+		nodo^.datos:= r;
 		act:= l;
 		while (act <> l) and (act^.datos.codObra < r.codObra) do begin
 			ant:= act;
 			act:= act^.sig;
 		end;
 		if (act = l) then
-			l:= nodo;
+			l:= nodo
 		else
 			ant^.sig:= nodo;
 		nodo^.sig:= act;
@@ -85,64 +94,71 @@ Var
 	entrada: reg_entrada;
 Begin
 	leerReg(entrada);
-	while (entrada.codObra <> 0) do begin
-		insertarOrdenado(v[entrada.dia], entrada);
+	while (entrada.codObra <> FIN) do begin
+		insertarOrdenado(vDias[entrada.dia], entrada);
 		leerReg(entrada);
 	end;
 End;
 //__________________________________PROCEDIMIENTOS__________________________________
-Procedure GenerarLista(var l: lista; vec: vec_dias);	{MergeAcumulador}
+Procedure GenerarLista(var l: lista_obras; vec: vec_dias);	{MergeAcumulador}
 
 	procedure minimo(var v: vec_dias; var minC: integer);
 	var
-		posMin, i: rng_dias;
+		posMin, i: rango_dias;
 	begin
-		minC:= 999;
-		for i:= 1 to MAXdias do
-			if ((v[i]) <> nil) and (v[i]^.datos.codObra < minC)) then begin
+		minC:= VALOR_ALTO;
+		
+		for i:= 1 to MAX_dias do
+			if (v[i] <> nil) and (v[i]^.datos.codObra < minC) then begin
 				posMin:= i;
 				minC:= v[i]^.datos.codObra;
 			end;
-		if (minC <> 999) then
+			
+		if (minC <> VALOR_ALTO) then
 			v[posMin]:= v[posMin]^.sig;
 	end;
 	
-	procedure agregarAlFinal (var pri: lista_obras; r: reg_obra);
+	procedure agregarAtras (var l: lista_obras; r: reg_obra);
 	var 
-		act, nue : lista;
+		act, nodo : lista_obras;
 	begin
-		new (nue);
-		nue^.datos:= per;
-		nue^.sig := NIL;
-		if pri <> Nil then begin
-			act := pri ;
-			while (act^.sig <> NIL ) do act := act^.sig ;
-				act^.sig := nue ;
+		new (nodo);
+		nodo^.datos:= r;
+		nodo^.sig := nil;
+		if (l <> nil) then begin
+			act := l;
+			while (act^.sig <> nil) do 
+			    act := act^.sig ;
+			act^.sig := nodo;
 		end
 		else
-			pri:= nue;
+			l:= nodo;
 	end;
 
 Var
-	minCod: integer;;
+	minCod: integer;
 	act: reg_obra;
 	
 Begin
 	l:= nil;
+	
 	minimo(vec, minCod);
-	while (minCod <> 999) do begin
+	
+	while (minCod <> VALOR_ALTO) do begin
 		act.codObra:= minCod;
 		act.entradasVendidas:= 0;
-		while (minCod <> 999) and (minCod  = act.codObra) do begin
+		
+		while (minCod <> VALOR_ALTO) and (minCod = act.codObra) do begin
 			act.entradasVendidas:= act.entradasVendidas + 1;
 			minimo(vec, minCod);
-		end
-		agregarAlFinal(l, act);
+		end;
+		
+		agregarAtras(l, act);
 	end;
 
 End;
 //__________________________________MostrarLista__________________________________
-Procedure MostrarLista(l: lista);	{Recursivo}
+Procedure MostrarLista(l: lista_obras);	{Recursivo}
 Begin
 	if (l <> nil) then begin
 		writeln('Codigo de Obra: ', l^.datos.codObra);
@@ -154,7 +170,7 @@ End;
 //__________________________________P.P__________________________________
 VAR
 	vector: vec_dias;
-	l: lista_obras
+	l: lista_obras;
 BEGIN
 	InicializarVec(vector);
 	GenerarVec(vector);
--- Practica1-Imperativo/Ejercicios/adicional3.pas
+++ Practica1-Imperativo/Ejercicios/adicional3.pas
@@ -1,26 +1,29 @@
-{3. El administrador de un edificio de oficinas, cuenta en papel, con la información 
+{
+3. El administrador de un edificio de oficinas, cuenta en papel, con la información 
 del pago de las expensas de dichas oficinas.
 Implementar un programa que:
-a. Genere un vector, sin orden, con a lo sumo las 300 oficinas que administra. De 
-cada oficina se ingresa código de identificación, DNI del propietario y valor de 
-la expensa. La lectura finaliza cuando llega el código de identificación -1.
-b. Ordene el vector, aplicando alguno de los métodos vistos en la teoría, para 
-obtener el vector ordenado por código de identificación de la oficina.
-c. Realice una búsqueda dicotómica que recibe el vector generado en b) y un 
-código de identificación de oficina y retorne si dicho código está en el vector. 
-En el caso de encontrarlo, se debe informar el DNI del propietario y en caso 
-contrario informar que el código buscado no existe.}
+	a. Genere un vector, sin orden, con a lo sumo las 300 oficinas que administra. 
+	De cada oficina se ingresa código de identificación, DNI del propietario y valor 
+	de la expensa. La lectura finaliza cuando llega el código de identificación -1.
+	b. Ordene el vector, aplicando alguno de los métodos vistos en la teoría, para 
+	obtener el vector ordenado por código de identificación de la oficina.
+	c. Realice una búsqueda dicotómica que recibe el vector generado en b) y un 
+	código de identificación de oficina y retorne si dicho código está en el vector. 
+	En el caso de encontrarlo, se debe informar el DNI del propietario y en caso 
+	contrario informar que el código buscado no existe.
+}
 
 PROGRAM Adicional3;
 CONST 
-	MAXoficinas = 300;
+	MAX_oficinas = 300;
+    FIN = -1;
 	
 TYPE
-	rng_oficinas = 1..300;
-	indice = 0..300;
+	rng_oficinas = 1..MAX_oficinas;
+	indice = 0..MAX_oficinas;
 	
 	reg_oficina = record
-		codId: rng_oficinas;
+		codId: integer;
 		dniPropietario: integer;
 		valorExpensa: real;
 	end;
@@ -29,13 +32,13 @@ TYPE
 
 //_____________________________GenerarVector_____________________________
 
-Procedure GenerarVec(var vec: vec_oficinas; var dimL: indice);
+Procedure GenerarVec(var v: vec_oficinas; var dimL: indice);
 	
 	procedure leerReg(var r: reg_oficina);
 	begin
 		with r do begin
 			readln(codId);
-			if (codId <> -1) then begin
+			if (codId <> FIN) then begin
 				readln(dniPropietario);
 				readln(valorExpensa);
 			end;
@@ -47,7 +50,7 @@ Var
 Begin
 	dimL:= 0;
 	leerReg(oficina);
-	while (oficina.codId <> -1) and (dimL < 300) do begin
+	while (oficina.codId <> FIN) and (dimL < MAX_oficinas) do begin
 		dimL:= dimL + 1;
 		v[dimL]:= oficina;
 		leerReg(oficina);
@@ -57,7 +60,8 @@ End;
 //_____________________________OrdenarVector_____________________________
 Procedure OrdenarVec(var v: vec_oficinas; dimL: rng_oficinas);		{Incersion}
 Var
-	i, j, act: indice;
+	i: rng_oficinas;
+	j, act: indice;
 Begin
 	for i:= 2 to dimL do begin
 		act:= i;
@@ -69,25 +73,14 @@ Begin
 		v[j + 1]:= v[act];
 	end;
 End;
-//_____________________________EncontrarOficina_____________________________
-Procedure Encontrar(v: vec_oficinas; dimL: rng_oficinas);
-	procedure busquedaBinaria()
 
-Var
-	n: rng_oficinas;
-Begin
-	write('Ingrese CodId de Oficina para eliminar: ');
-	readln(n);
-	busquedaBinaria(v, dimL, n)
-	
-End;
+
 
 //_____________________________PP_____________________________
 VAR
 	vector: vec_oficinas;
-	dimL: indice
+	dimL: indice;
 BEGIN
 	GenerarVec(vector, dimL);
 	OrdenarVec(vector, dimL);
-	Encontrar(vector, dimL);
 END.
